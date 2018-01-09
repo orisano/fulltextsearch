2d78dec32584683f7089aa3d2c158861d7052c75
Nao YONASHIRO <owan.orisano@gmail.com>
2018-01-10 08:41:12 +0900

    feat: change Tokenize signature

--- token.go
+++ token.go
@@ -1,22 +1,34 @@
 package fulltextsearch
 
+import "unicode/utf8"
+
+type Token struct {
+	Text   string
+	Offset int
+}
+
 type Tokenizer interface {
-	Tokenize(string) []string
+	Tokenize(string) []*Token
 }
 
 type NgramTokenizer struct {
 	N int
 }
 
-func (nt *NgramTokenizer) Tokenize(s string) []string {
+func (nt *NgramTokenizer) Tokenize(s string) []*Token {
 	r := []rune(s)
 	t := len(r) - nt.N + 1
 	if t <= 0 {
 		return nil
 	}
-	tokens := make([]string, 0, t)
+	tokens := make([]*Token, 0, t)
+	offset := 0
 	for i := 0; i < t; i++ {
-		tokens = append(tokens, string(r[i:i+nt.N]))
+		tokens = append(tokens, &Token{
+			Text:   string(r[i : i+nt.N]),
+			Offset: offset,
+		})
+		offset += utf8.RuneLen(r[i])
 	}
 	return tokens
 }
--- token_test.go
+++ token_test.go
@@ -2,12 +2,15 @@ package fulltextsearch
 
 import "testing"
 
-func equals(a, b []string) bool {
+func equals(a, b []*Token) bool {
 	if len(a) != len(b) {
 		return false
 	}
 	for i := range a {
-		if a[i] != b[i] {
+		if a[i].Text != b[i].Text {
+			return false
+		}
+		if a[i].Offset != b[i].Offset {
 			return false
 		}
 	}
@@ -18,27 +21,42 @@ func TestNgramTokenizer_Tokenize(t *testing.T) {
 	tests := []struct {
 		n        int
 		text     string
-		expected []string
+		expected []*Token
 	}{
 		{
-			n:        3,
-			text:     "foobar",
-			expected: []string{"foo", "oob", "oba", "bar"},
+			n:    3,
+			text: "foobar",
+			expected: []*Token{
+				{"foo", 0},
+				{"oob", 1},
+				{"oba", 2},
+				{"bar", 3},
+			},
 		},
 		{
-			n:        2,
-			text:     "検索エンジン",
-			expected: []string{"検索", "索エ", "エン", "ンジ", "ジン"},
+			n:    2,
+			text: "検索エンジン",
+			expected: []*Token{
+				{"検索", 0},
+				{"索エ", len("検")},
+				{"エン", len("検索")},
+				{"ンジ", len("検索エ")},
+				{"ジン", len("検索エン")},
+			},
 		},
 		{
-			n:        2,
-			text:     "𩸽の定食",
-			expected: []string{"𩸽の", "の定", "定食"},
+			n:    2,
+			text: "𩸽の定食",
+			expected: []*Token{
+				{"𩸽の", 0},
+				{"の定", len("𩸽")},
+				{"定食", len("𩸽の")},
+			},
 		},
 		{
 			n:        10,
 			text:     "less",
-			expected: []string{},
+			expected: []*Token{},
 		},
 	}
 	for _, test := range tests {
